5b1d736cd2af3e03f38dd549ea9c4adac94cdf14
babasiker34 <53488423+babasiker34@users.noreply.github.com>
2019-07-30 19:48:32 +0300

    Update and rename lineage_zangyapro.mk to lineage_gm9pro.mk

---
+++ lineage_gm9pro.mk
@@ -0,0 +1,20 @@
+# Inherit some common Lineage stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Vendor blobs
+$(call inherit-product-if-exists, vendor/gm/gm9pro/gm9pro-vendor.mk)
+
+# Device
+$(call inherit-product, device/gm/gm9pro/device.mk)
+
+# Device identifiers
+PRODUCT_DEVICE := gm9pro
+PRODUCT_NAME := lineage_gm9pro
+PRODUCT_BRAND := gm
+PRODUCT_MODEL := General Mobile GM 9 Pro
+PRODUCT_MANUFACTURER := gm
+PRODUCT_RELEASE_NAME := gm9pro
